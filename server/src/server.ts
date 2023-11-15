8b274aadf3fffd01d3b77fa3eb2ab98558d6522b
ColemanCook <colemancook@outlook.com>
2023-11-15 11:14:20 -0700

    Adding routes to typescript app

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,10 +1,16 @@
 import express, { Request, Response } from 'express';
+import userRoutes from './routes/userRoutes';
 
 // Create an instance of express
 const app = express();
 
 // Define the port the server will run on
 const PORT = process.env.PORT || 3000;
+
+// Middleware to parse request body
+app.use(express.json());
+
+app.use('/users', userRoutes);
 
 // Define a route handler for GET requests to the root URL ('/')
 app.get('/', (req: Request, res: Response) => {
