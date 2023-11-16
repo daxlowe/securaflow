de6a0c477d5984aac68cc4ce0cb3d7f2fdb0c3a4
Shad Boswell <u1057240@utah.edu>
2023-11-15 22:26:04 -0700

    Changed models to typescript, added connection string information, and connection call to MongoDB in Server.ts

---
+++ server/dist/models/Ticket.js
@@ -0,0 +1,66 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const statusSchema = new mongoose_1.Schema({
+    body: {
+        type: String,
+        required: true
+    },
+    date_started: {
+        type: Date,
+        required: true
+    },
+    date_ended: {
+        type: Date
+    }
+});
+const ticketSchema = new mongoose_1.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    difficulty: {
+        type: Number,
+        required: true
+    },
+    assignees: {
+        type: [{ type: mongoose_1.Schema.Types.ObjectId, ref: 'User' }]
+    },
+    time_estimate: {
+        type: Number
+    },
+    current_status: {
+        type: String,
+        required: true
+    },
+    status_updates: {
+        type: [statusSchema]
+    },
+    vulnerability: {
+        name: {
+            type: String,
+            required: true
+        },
+        cve_id: {
+            type: String,
+            required: true
+        },
+        priority: {
+            type: String,
+            required: true
+        },
+        imported_from: {
+            type: String,
+            required: true
+        }
+    },
+    comments: {
+        type: [String]
+    }
+});
+const Ticket = (0, mongoose_1.model)('Ticket', ticketSchema);
+exports.default = Ticket;
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,20 @@
 import express, { Request, Response } from 'express';
 import userRoutes from './routes/userRoutes';
+import * as mongoose from 'mongoose';
+import dotenv from 'dotenv'
+import User from './models/User';
+import Ticket from './models/Ticket';
+import Group from './models/Group';
+import { group } from 'console';
+
+dotenv.config()
 
 // Create an instance of express
 const app = express();
 
 // Define the port the server will run on
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT;
+const DB_CONN_STRING: string = `${process.env.DB_CONN}${process.env.DB_NAME}${process.env.DB_OPTIONS}`;
 
 // Middleware to parse request body
 app.use(express.json());
@@ -13,11 +22,56 @@ app.use(express.json());
 app.use('/users', userRoutes);
 
 // Define a route handler for GET requests to the root URL ('/')
-app.get('/', (req: Request, res: Response) => {
-    res.send('Hello World!');
+app.get('/', async (req: Request, res: Response) => {
+    try
+    {
+        //res.status(200).send("Hello, World!");
+        const ticket = await Ticket.create(
+            {
+                name: "Leave Swamp", 
+                description: "You need to leave Shrek's swamp ASAP", 
+                difficulty: 10, 
+                assignees: [], 
+                time_estimate: 10, 
+                current_status: "Open", 
+                status_updates: 
+                [
+                    {
+                        body: "Open", 
+                        date_started: "2001-04-22", 
+                        date_ended: null
+                    }
+                ], 
+                vulnerability:
+                {
+                    name: "GET OUT ME SWAMP", 
+                    cve_id: "1", 
+                    priority: "High", 
+                    imported_from: "Shrek"
+                }, 
+                comments: ["Bruh, you've gotta go"]
+            });
+
+        res.status(200).json(ticket);
+        //const user = await User.create({first_name: "Shad", last_name: "Boswell", email: "fakemail@gmail.com", groups: [], tickets: []});
+        //res.status(200).json(user);
+    }
+    catch (error: any)
+    {
+        res.status(400).json({error: error.message });
+    }
 });
 
 // Start the server
-app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-});
+
+mongoose.connect(DB_CONN_STRING)
+    .then(() =>
+	{
+	    app.listen(PORT, () => {
+		console.log(`Connected to DB. Server is running on port ${PORT}`);
+	    });
+	})
+    .catch((error) =>
+	{
+	    console.log(error);
+	});
