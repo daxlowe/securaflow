949847f90380612f555ec260a749413acc6a9f6d
ColemanCook <colemancook@outlook.com>
2023-11-22 01:53:11 -0700

    hacking together login and dashboard page for demo

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,6 +7,7 @@ import User from './models/User';
 import Ticket from './models/Ticket';
 import Group from './models/Group';
 import { group } from 'console';
+import path from 'path';
 
 dotenv.config()
 
@@ -17,11 +18,33 @@ const app = express();
 const PORT = process.env.PORT;
 const DB_CONN_STRING: string = `${process.env.DB_CONN}${process.env.DB_NAME}${process.env.DB_OPTIONS}`;
 
+// Serve static files from the 'static' directory
+app.use(express.static(path.join(__dirname, 'static')));
+
 // Middleware to parse request body
 app.use(express.json());
 
 app.use('/users', userRoutes);
 app.use('/tickets', ticketRoutes)
+
+app.get('/login', (req: Request, res: Response) => {
+    // __dirname is the directory of the current module, i.e., where server.ts is located
+    // path.join is used to create a path to your HTML file
+    res.sendFile(path.join(__dirname, 'static', 'hackedLoginYOLO.html'));
+});
+
+// Temporary login handler to redirect to the dashboard
+app.post('/login', (req: Request, res: Response) => {
+    // Redirect to the dashboard route
+    res.redirect('/dashboard');
+});
+
+// Define a route handler for the dashboard
+app.get('/dashboard', (req: Request, res: Response) => {
+    // Send the dashboard.html file
+    res.sendFile(path.join(__dirname, 'static', 'dashboard.html'));
+});
+
 // Define a route handler for GET requests to the root URL ('/')
 app.get('/', async (req: Request, res: Response) => {
     try
