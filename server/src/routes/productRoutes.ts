bde040b780e75ab09883210d293d3fd7ebc2807a
ColemanCook <colemancook@outlook.com>
2023-11-21 23:47:14 -0700

    put controllers in src folder, update .gitignore

--- server/src/routes/productRoutes.ts
+++
@@ -1,8 +0,0 @@
-import express, { Request, Response } from 'express';
-// Import other necessary modules, like controllers or middleware
-
-const router = express.Router();
-
-router.get('/', async (req: Request, res: Response) => {
-    
-})
