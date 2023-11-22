aba43cc86f3166b4ec0890ac9c80eec6eae3304e
Shad Boswell <u1057240@utah.edu>
2023-11-22 00:16:59 -0700

    Recreated React app

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,17 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css'
 
-import { BrowserRouter } from "react-router-dom";
-import "@/assets/css/index.css"
-//import ThemeProvider from "@mui/material/styles";
-
-import App from "./routes";
-
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <App />
   </React.StrictMode>,
-  document.getElementById("root"),
-);
+)
