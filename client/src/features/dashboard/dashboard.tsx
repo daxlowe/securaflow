42b7f015b6d175639e868e44e751f1499bcb25ef
Sam Bennett <sambennettdev@gmail.com>
2023-11-27 11:16:47 -0700

    created dashboard page

---
+++ client/src/features/dashboard/dashboard.tsx
@@ -0,0 +1,12 @@
+import '@/assets/css/App.css'
+
+function App() {
+
+    return (
+        <>
+            <h1>DASHBOARD</h1>
+        </>
+    )
+}
+
+export default App
--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Home from "@/features/home"
+import Dashboard from "@/features/dashboard"
 export default function App() {
     return (
         <div>
             <Routes>
                 <Route path="/home" element={<Home />} />
+                <Route path="/" element={<Dashboard />} />
             </Routes>
         </div>
     );
