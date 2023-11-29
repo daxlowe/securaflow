6515fbcb13c944f0c96efdf21c256e591a1b902f
Dax Lowe <daxton.lowe@gmail.com>
2023-11-29 11:25:06 -0700

    Filled out modifyTicket with fetch command

--- client/src/utils/modifyTicket.ts
+++ client/src/utils/modifyTicket.ts
@@ -1,5 +1,12 @@
 import { Ticket } from "@/types";
 
 export const modifyTicket = (ticket: Ticket) => {
-    
+    const options = {
+        method: "PATCH",
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(ticket)
+    };
+    fetch("http://localhost:3000/", options)
 }
