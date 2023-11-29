85173e197f636f496e12d64f96f7b1e40aeaeae5
Sam Bennett <sambennettdev@gmail.com>
2023-11-29 15:46:45 -0700

    modified table

--- client/src/features/dashboard/components/Tickets/Tickets.tsx
+++ client/src/features/dashboard/components/Tickets/Tickets.tsx
@@ -15,13 +15,45 @@ const Tickets: React.FC = () => {
         fetchData();
     }, []); // Empty dependency array to run the effect once on component mount
 
+    const priorityComparator = (a: 'High' | 'Moderate' | 'Low', b: 'High' | 'Moderate' | 'Low'): number => {
+        const priorityOrder: Record<'High' | 'Moderate' | 'Low', number> = {
+            High: 1,
+            Moderate: 2,
+            Low: 3,
+        };
+
+        const priorityA = priorityOrder[a];
+        const priorityB = priorityOrder[b];
+
+        return priorityA - priorityB;
+    };
+
+    const getPriorityColor = (priority: 'High' | 'Moderate' | 'Low'): string => {
+        switch (priority) {
+            case 'High':
+                return 'red';
+            case 'Moderate':
+                return 'orange';
+            case 'Low':
+                return 'green';
+            default:
+                return 'black';
+        }
+    };
 
     const columns: GridColDef[] = [
-        { field: 'title', headerName: 'Title', flex: 2, },
-        { field: 'priority', headerName: 'Priority', flex: 1, },
-        { field: 'status', headerName: 'Status', flex: 1, },
-        { field: 'team', headerName: 'Team', flex: 1, },
-        { field: 'assignee', headerName: 'Assignee', flex: 1, },
+        { field: 'title', headerName: 'Title', flex: 2 },
+        {
+            field: 'priority', headerName: 'Priority', flex: 1, sortComparator: priorityComparator,
+            renderCell: (params) => (
+                <div style={{ color: getPriorityColor(params.value as 'High' | 'Moderate' | 'Low') }}>
+                    {params.value}
+                </div>
+            ),
+        },
+        { field: 'status', headerName: 'Status', flex: 1 },
+        { field: 'team', headerName: 'Team', flex: 1 },
+        { field: 'assignee', headerName: 'Assignee', flex: 1 },
     ];
 
     const rows = vulnerabilityTickets.map((ticket) => ({
@@ -40,6 +72,13 @@ const Tickets: React.FC = () => {
             rowHeight={72}
             disableColumnMenu
             checkboxSelection
+            initialState={{
+                sorting: {
+                    sortModel: [{ field: 'priority', sort: 'asc' }],
+                },
+                pagination: { paginationModel: { pageSize: 5 } },
+            }}
+            pageSizeOptions={[5, 10, 25]}
             sx={{
                 '& .MuiDataGrid-cell:hover': {
                     color: 'secondary.main',
