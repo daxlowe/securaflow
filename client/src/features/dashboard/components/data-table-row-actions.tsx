3f9f4049a801fc39ffa5a07027e71676069a28d9
Dax Lowe <daxton.lowe@gmail.com>
2023-12-06 22:18:17 -0700

    Abstracted ticket views out and render in view

--- client/src/features/dashboard/components/data-table-row-actions.tsx
+++ client/src/features/dashboard/components/data-table-row-actions.tsx
@@ -1,21 +1,18 @@
 import { DotsHorizontalIcon } from "@radix-ui/react-icons";
 import { Row } from "@tanstack/react-table";
 import { Button } from "@/components/ui/button";
+import { useState } from "react";
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
-  DropdownMenuRadioGroup,
-  DropdownMenuRadioItem,
-  DropdownMenuSeparator,
   DropdownMenuShortcut,
-  DropdownMenuSub,
-  DropdownMenuSubContent,
-  DropdownMenuSubTrigger,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-import { labels } from "../data/data";
-import { Ticket } from "@/types";
+import { ViewTicket } from "@/components/ViewTicketPopup";
+import { ModifyTicket } from "@/components/ModifyTicketPopup";
+import { CreateTicket } from "@/components/CreateTicketPopup";
+import { Task } from "../types"
 
 interface DataTableRowActionsProps<TData> {
   row: Row<TData>;
@@ -24,44 +21,51 @@ interface DataTableRowActionsProps<TData> {
 export function DataTableRowActions<TData>({
   row,
 }: DataTableRowActionsProps<TData>) {
-  const originalData = row.original as Ticket;
+  const originalData = row.original as Task;
   // Now you can safely use the data
   const task = originalData;
+  
+  enum ComponentTypes {
+    ViewTicket = 'viewTicket',
+    ModifyTicket = 'modifyTicket',
+    CreateTicket = 'createTicket'
+  }
 
-  const handleViewClick = () => {
-    console.log("View clicked for task:", task);
-    // Add your view logic here
-  };
+  type ActiveComponentType = ComponentTypes | null;
+  
+    //State to track which component to display
+    const [activeComponent, setActiveComponent] = useState<ActiveComponentType>(null);
+  
+    const handleMenuClick = (componentName: ComponentTypes) => {
+      setActiveComponent(componentName);
+    };
 
-  const handleEditClick = () => {
-    console.log("Edit clicked for task:", task);
-    // Add your edit logic here
-  };
+    const components: Record<ComponentTypes, JSX.Element> = {
+      [ComponentTypes.ViewTicket]: <ViewTicket task={task} />,
+      [ComponentTypes.CreateTicket]: <CreateTicket />,
+      [ComponentTypes.ModifyTicket]: <ModifyTicket task={task} />,
+    };
 
-  const handleDeleteClick = () => {
-    console.log("Delete clicked for task:", task);
-    // Add your delete logic here
-  };
-
-  return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button
-          variant="ghost"
-          className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
-        >
-          <DotsHorizontalIcon className="h-4 w-4" />
-          <span className="sr-only">Open menu</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end" className="w-[160px]">
-        <DropdownMenuItem onClick={handleViewClick}>View</DropdownMenuItem>
-        <DropdownMenuItem onClick={handleEditClick}>Edit</DropdownMenuItem>
-        <DropdownMenuItem onClick={handleDeleteClick}>
-          Delete
-          <DropdownMenuShortcut>⌘⌫</DropdownMenuShortcut>
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
-  );
+    return (
+      <>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button
+            variant="ghost"
+            className="flex h-8 w-8 p-0 data-[state=open]:bg-muted"
+          >
+            <DotsHorizontalIcon className="h-4 w-4" />
+            <span className="sr-only">Open menu</span>
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end" className="w-[160px]">
+          <DropdownMenuItem onClick={() => handleMenuClick(ComponentTypes.ViewTicket)}>View</DropdownMenuItem>
+          <DropdownMenuItem onClick={() => handleMenuClick(ComponentTypes.ModifyTicket)}>Edit</DropdownMenuItem>
+          <DropdownMenuItem onClick={() => handleMenuClick(ComponentTypes.CreateTicket)}>Create</DropdownMenuItem>
+          <DropdownMenuItem>Delete<DropdownMenuShortcut>⌘⌫</DropdownMenuShortcut></DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
+      {activeComponent && components[activeComponent]}
+      </>
+    );
 }
