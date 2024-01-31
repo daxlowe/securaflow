b7dbcc0c6222d4c3c75bdccdd5e39031c35209a4
Sam Bennett <sambennettdev@gmail.com>
2024-01-30 20:35:58 -0700

    added navbar

--- client/src/features/account-settings/account-settings.tsx
+++ client/src/features/account-settings/account-settings.tsx
@@ -2,15 +2,31 @@ import { useLocation } from "react-router-dom"; // Assuming you have react-route
 import SettingsLayout from "./layout";
 import { ProfileForm } from "./profile/profile-form";
 import { AppearanceForm } from "./appearance/appearance-form";
+import { MainNav } from "@/components/navbar/main-nav";
+import { Search } from "@/components/navbar/search";
+import { UserNav } from "@/components/navbar/user-nav";
 
 export default function Settings() {
   const location = useLocation();
   const pathname = location.pathname;
 
   return (
-    <SettingsLayout>
-      {pathname === "/settings/profile" && <ProfileForm />}
-      {pathname === "/settings/appearance" && <AppearanceForm />}
-    </SettingsLayout>
+    <>
+      <div className="hidden flex-col md:flex">
+        <div className="border-b">
+          <div className="flex h-16 items-center px-4">
+            <MainNav className="mx-6" />
+            <div className="ml-auto flex items-center space-x-4">
+              <Search />
+              <UserNav />
+            </div>
+          </div>
+        </div>
+      </div>
+      <SettingsLayout>
+        {pathname === "/settings/profile" && <ProfileForm />}
+        {pathname === "/settings/appearance" && <AppearanceForm />}
+      </SettingsLayout>
+    </>
   );
 }
