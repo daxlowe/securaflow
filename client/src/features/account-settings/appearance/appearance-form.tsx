00fcd044e7c4fee60abf5ce6076ac7e37f013bc1
Sam Bennett <sambennettdev@gmail.com>
2024-02-07 19:06:52 -0700

    removed toast

--- client/src/features/account-settings/appearance/appearance-form.tsx
+++ client/src/features/account-settings/appearance/appearance-form.tsx
@@ -35,14 +35,6 @@ export function AppearanceForm() {
   function onSubmit(data: AppearanceFormValues) {
     // Save the selected theme to local storage
     setTheme(data.theme);
-    toast({
-      title: "You submitted the following values:",
-      description: (
-        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
-        </pre>
-      ),
-    });
   }
 
   return (
