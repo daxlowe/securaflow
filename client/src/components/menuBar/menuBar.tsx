b24565363b19bc10ace441671b80b8dbc8926124
Sam Bennett <sambennettdev@gmail.com>
2024-02-12 18:49:09 -0700

    improved Organization

--- client/src/components/menuBar/menuBar.tsx
+++ client/src/components/menuBar/menuBar.tsx
@@ -9,7 +9,7 @@ import {
 
 const MenuBar: React.FC = () => {
   return (
-    <div className="flex flex-col justify-between h-screen p-4">
+    <div className="flex flex-col justify-between h-screen p-4 w-fit">
       <div className="list-none">
         <NavigationMenu className="flex flex-col">
           <NavigationMenuItem className="mb-1">
